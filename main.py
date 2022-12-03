5572c32d76dcd76063ba649bc9601fd5fe3827d6
tanvikad <tanvika.dasari@gmail.com>
2022-12-03 14:08:26 -0800

    added a translation package

--- main.py
+++ main.py
@@ -10,3 +10,8 @@ doc = nlp("I was reading the paper.")
 token = doc[0]  # 'I'
 print(token.morph)  # 'Case=Nom|Number=Sing|Person=1|PronType=Prs'
 print(token.morph.get("PronType"))  # ['Prs']
+
+
+from deep_translator import GoogleTranslator
+translated = GoogleTranslator(source='auto', target='de').translate("keep it up, you are awesome")  # output -> Weiter so, du bist großartig
+print(translated)
